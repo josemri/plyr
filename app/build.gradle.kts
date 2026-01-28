d8ced0f755849710c7310612c21a51126791f972
josemri <josepm.ramos.i@gmail.com>
2026-01-28 15:38:37 +0100

    i forgor 💀

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         //noinspection OldTargetApi
         targetSdk = 35
         versionCode = 4
-        versionName = "1.0.4"
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
